5b502175394c06c9621540d05a090e440ef6097e
bashrc <bob@libreserver.org>
2026-03-20 16:09:12 +0000

    License header tidying

--- src/iec16022ecc200.h
+++ src/iec16022ecc200.h
@@ -1,18 +1,25 @@
-/*
-  IEC16022 encoder, see IEC16022
-  This software is provided under the terms of the GPL v2 or later.
-  This software is provided free of charge with a full "Money back" guarantee.
-  Use entirely at your own risk. We accept no liability. If you don't like that - don't use it.
-
-  $Log: iec16022ecc200.h,v $
-  Revision 1.4  2006/01/25 11:10:43  cvs
-  New sqlhtml
-
-  Revision 1.3  2004/09/09 07:45:09  cvs
-  Added change history to source files
-  Added "info" type to IEC16022
-  Added exact size checking shortcodes on encoding generation for iec16022
-*/
+/*********************************************************************
+ * Software License Agreement (GPLv2)
+ *
+ * IEC16022 bar code generation library
+ * This software is provided under the terms of the GPL v2 or later.
+ * This software is provided free of charge with a full "Money back" guarantee.
+ * Use entirely at your own risk. We accept no liability.
+ * If you don't like that - don't use it.
+ *
+ * Adrian Kennard, Andrews & Arnold Ltd
+ * with help from Cliff Hones on the RS coding
+ *
+ * $Log: iec16022ecc200.h,v $
+ * Revision 1.4  2006/01/25 11:10:43  cvs
+ * New sqlhtml
+ *
+ * Revision 1.3  2004/09/09 07:45:09  cvs
+ * Added change history to source files
+ * Added "info" type to IEC16022
+ * Added exact size checking shortcodes on encoding generation for iec16022
+ *
+ *********************************************************************/
 
 #define MAXBARCODE 3116
 
