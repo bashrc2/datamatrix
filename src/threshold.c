e851da76bfc3423798deb0537ff2b8047eb9caea
bashrc <bob@libreserver.org>
2026-05-11 17:41:53 +0100

    Tidying

--- src/threshold.c
+++ src/threshold.c
@@ -80,7 +80,7 @@ int meanlight_threshold(unsigned char img[], int width, int height,
   unsigned char * img_mono = thresholded;
   unsigned char dark=0,light=0,range;
   unsigned int percent_active=0;
-  unsigned char thresh;
+  unsigned char thresh, is_dark;
   int i, percent;
 
   colour_to_mono(img, width, height, bitsperpixel, img_mono);
@@ -94,21 +94,14 @@ int meanlight_threshold(unsigned char img[], int width, int height,
   thresh = (unsigned char)(dark + (range / 2));
   /* adapt the threshold to the dynamic range */
   threshold = threshold * (int)range / 100;
-  if (is_dark_image(img_mono, width, height, dark, light)) {
+  /* is the image mostly light or mostly dark? */
+  is_dark = is_dark_image(img_mono, width, height, dark, light);
+  if (is_dark) {
     /* Image is mostly light, count the dark pixels */
     if ((int)dark + threshold > 255)
       thresh = (unsigned char)255;
     else
       thresh = (unsigned char)((int)dark + threshold);
-
-    for (i = (width*height) - 1; i >= 0; i--)
-      if (img_mono[i] < thresh) {
-        thresholded[i] = 255;
-        percent_active++;
-      }
-      else {
-        thresholded[i] = 0;
-      }
   }
   else {
     /* Image is mostly dark, count the light pixels */
@@ -116,16 +109,19 @@ int meanlight_threshold(unsigned char img[], int width, int height,
       thresh = (unsigned char)0;
     else
       thresh = (unsigned char)((int)light - threshold);
-
-    for (i = (width*height) - 1; i >= 0 ; i--)
-      if (img_mono[i] >= thresh) {
-        thresholded[i] = 255;
-        percent_active++;
-      }
-      else {
-        thresholded[i] = 0;
-      }
   }
+
+  for (i = (width*height) - 1; i >= 0 ; i--) {
+    if (((!is_dark) && (img_mono[i] >= thresh)) ||
+        ((is_dark) && (img_mono[i] < thresh))) {
+      thresholded[i] = 255;
+      percent_active++;
+    }
+    else {
+      thresholded[i] = 0;
+    }
+  }
+
   percent = (int)(percent_active*100/(width*height));
   if (percent > 30) {
     /* invert the thresholded image */
