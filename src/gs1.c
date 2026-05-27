9a006fc1ff2079cdcd99e294a11acc1dfad3743c
bashrc <bob@libreserver.org>
2026-05-27 12:49:33 +0100

    Formatting

--- src/gs1.c
+++ src/gs1.c
@@ -4857,7 +4857,7 @@ void gs1_semantics(char result[],
                         /* first ten characters are latitude */
                         for (dec_ctr = 0; dec_ctr < 10; dec_ctr++) {
                             if ((data_str[dec_ctr] < '0') ||
-                                    (data_str[dec_ctr] > '9')) break;
+                                (data_str[dec_ctr] > '9')) break;
                             latlon_str[dec_ctr] = data_str[dec_ctr];
                         }
                         latlon_str[dec_ctr] = 0;
@@ -4868,7 +4868,7 @@ void gs1_semantics(char result[],
                             dec_ctr = 0;
                             for (dec_ctr = 0; dec_ctr < 10; dec_ctr++) {
                                 if ((data_str[dec_ctr] < '0') ||
-                                        (data_str[dec_ctr] > '9')) break;
+                                    (data_str[dec_ctr] > '9')) break;
                                 latlon_str[dec_ctr] = data_str[dec_ctr + 10];
                             }
                             latlon_str[dec_ctr] = 0;
@@ -5172,7 +5172,7 @@ void gs1_semantics(char result[],
                     decode_strcat(gs1_result, "AIDC MEDIA TYPE: ");
                     if ((int)strlen(data_str) == 2) {
                         if ((data_str[0] >= '0') && (data_str[0] <= '9') &&
-                                (data_str[1] >= '0') && (data_str[1] <= '9')) {
+                            (data_str[1] >= '0') && (data_str[1] <= '9')) {
                             aidc_media_type = atoi(data_str);
                         }
                     }
@@ -5229,7 +5229,7 @@ void gs1_semantics(char result[],
                     decode_strcat(gs1_result, "BIRTH SEQUENCE: ");
                     if ((int)strlen(data_str) == 3) {
                         if ((data_str[0] >= '0') && (data_str[0] <= '9') &&
-                                (data_str[2] >= '0') && (data_str[2] <= '9')) {
+                            (data_str[2] >= '0') && (data_str[2] <= '9')) {
                             birth_sequence[0] = data_str[0];
                             birth_sequence[1] = data_str[2];
                         }
@@ -5249,7 +5249,7 @@ void gs1_semantics(char result[],
                         unsigned char all_numbers = 1;
                         for (int dim_ctr = 0; dim_ctr < 14; dim_ctr++) {
                             if ((data_str[dim_ctr] < '0') ||
-                                    (data_str[dim_ctr] > '9')) {
+                                (data_str[dim_ctr] > '9')) {
                                 all_numbers = 0;
                                 break;
                             }
@@ -5375,13 +5375,13 @@ void gs1_semantics(char result[],
                     }
                     if ((int)strlen(data_str) == 18) {
                         if ((data_str[14] >= '0') && (data_str[14] <= '9') &&
-                                (data_str[15] >= '0') && (data_str[15] <= '9')) {
+                            (data_str[15] >= '0') && (data_str[15] <= '9')) {
                             itip_piece_number_str[0] = data_str[14];
                             itip_piece_number_str[1] = data_str[15];
                             itip_piece_number_str[2] = 0;
                         }
                         if ((data_str[16] >= '0') && (data_str[16] <= '9') &&
-                                (data_str[17] >= '0') && (data_str[17] <= '9')) {
+                            (data_str[17] >= '0') && (data_str[17] <= '9')) {
                             itip_total_count_str[0] = data_str[16];
                             itip_total_count_str[1] = data_str[17];
                             itip_total_count_str[2] = 0;
@@ -5543,13 +5543,13 @@ void gs1_semantics(char result[],
                     }
                     if ((int)strlen(data_str) == 18) {
                         if ((data_str[14] >= '0') && (data_str[14] <= '9') &&
-                                (data_str[15] >= '0') && (data_str[15] <= '9')) {
+                            (data_str[15] >= '0') && (data_str[15] <= '9')) {
                             itip_piece_number_str[0] = data_str[14];
                             itip_piece_number_str[1] = data_str[15];
                             itip_piece_number_str[2] = 0;
                         }
                         if ((data_str[16] >= '0') && (data_str[16] <= '9') &&
-                                (data_str[17] >= '0') && (data_str[17] <= '9')) {
+                            (data_str[17] >= '0') && (data_str[17] <= '9')) {
                             itip_total_count_str[0] = data_str[16];
                             itip_total_count_str[1] = data_str[17];
                             itip_total_count_str[2] = 0;
