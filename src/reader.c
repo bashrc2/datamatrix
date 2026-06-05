a0d3bc81557f89082e26c97272ab5bcaf597252f
bashrc <bob@libreserver.org>
2026-06-05 18:29:47 +0100

    Go large on dilation for undermarked parts

--- src/reader.c
+++ src/reader.c
@@ -140,23 +140,23 @@ int read_datamatrix(unsigned char image_data[],
     /* the magic numbers
        Note that these are doubled sets so that we can also vary the
        meanlight sampling radius*/
-    const int no_of_configs = 6*2;
+    const int no_of_configs = 7*2;
     int max_config = no_of_configs;
     int ml_threshold_configs[] = {
-        0, 0, 50, 10, 10, 20,
-        0, 0, 50, 10, 10, 20
+        0, 0, 50, 10, 10, 20, 10,
+        0, 0, 50, 10, 10, 20, 10
     };
     int erosion_itterations_configs[] = {
-        1, 0, 1, 0, 0, 0,
-        1, 0, 1, 0, 0, 0
+        1, 0, 1, 0, 0, 0, 0,
+        1, 0, 1, 0, 0, 0, 0
     };
     int dilate_itterations_configs[] = {
-        9, 5, 4, 6, 5, 2,
-        9, 5, 4, 6, 5, 2
+        9, 5, 4, 6, 5, 2, 15,
+        9, 5, 4, 6, 5, 2, 15
     };
     int ml_sampling_radius[] = {
-        50, 50, 50, 50, 50, 50,
-        20, 20, 20, 20, 20, 20
+        50, 50, 50, 50, 50, 50, 50,
+        20, 20, 20, 20, 20, 20, 20
     };
 
     struct grid_2d grid[no_of_configs];
@@ -1274,7 +1274,7 @@ int read_datamatrix(unsigned char image_data[],
                 show_quality_metrics(&grid[best_config], csv, json, yaml,
                                      aperture, light_nm, light_angle_degrees);
                 if (((int)strlen(report_template) > 0) &&
-					((int)strlen(report_filename) > 0)) {
+                    ((int)strlen(report_filename) > 0)) {
                     save_verification_report(&grid[best_config],
                                              address_line1,
                                              address_line2,
